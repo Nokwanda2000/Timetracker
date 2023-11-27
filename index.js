fc0b860ea0cb8f5602368e98a4a938481ff9373d
User <User@WRSD-LC310-023.dut.ac.za>
2023-11-27 15:02:00 +0200

    new

---
+++ index.js
@@ -0,0 +1,50 @@
+function Daily(){
+  document.getElementById("work").innerHTML = "5hrs"; 
+  document.getElementById("works").innerHTML = "yesterday - 7hrs";
+  document.getElementById("play").innerHTML = "1hr";
+  document.getElementById("plays").innerHTML = "yesterday - 2hrs" ;
+  
+  document.getElementById("study").innerHTML = "8hrs";
+  document.getElementById("studied").innerHTML = "yesterday - 1hr" ;
+  document.getElementById("exercise").innerHTML = "1hr" 
+  document.getElementById("exercises").innerHTML = "yesterday - 1hr" ;
+  document.getElementById("social").innerHTML = "1hrs" ;
+  document.getElementById("socials").innerHTML = "yesterday - 3hrs" ;
+  document.getElementById("self").innerHTML = "8hrs" 
+  document.getElementById("selfs").innerHTML = "yesterday - 1hr" ;
+    
+}
+function Weekly(){
+  document.getElementById("work").innerHTML = "32hrs"; 
+  document.getElementById("works").innerHTML = "lastweek - 36hrs";
+  document.getElementById("play").innerHTML = "10hr";
+  document.getElementById("plays").innerHTML = "lastweek - 8hrs" ;
+  
+  document.getElementById("study").innerHTML = "4hrs";
+  document.getElementById("studied").innerHTML = "lastweek - 7hr" ;
+  document.getElementById("exercise").innerHTML = "4hr" 
+  document.getElementById("exercises").innerHTML = "yesterday - 5hr" ;
+  document.getElementById("social").innerHTML = "5hrs" ;
+  document.getElementById("socials").innerHTML = "lastweek- 10hrs" ;
+  document.getElementById("self").innerHTML = "2hrs" ;
+  document.getElementById("selfs").innerHTML = "lastweek- 2hrs" ;
+    
+}
+
+function monthly(){
+  document.getElementById("work").innerHTML = "103hrs"; 
+  document.getElementById("works").innerHTML = "last month - 128hrs";
+  document.getElementById("play").innerHTML = "23hr";
+  document.getElementById("plays").innerHTML = "last month- 29hrs" ;
+  
+  document.getElementById("study").innerHTML = "13hrs";
+  document.getElementById("studied").innerHTML = "last month - 19hr" ;
+  document.getElementById("exercise").innerHTML = "11hr" 
+  document.getElementById("exercises").innerHTML = "last month - 18hr" ;
+  document.getElementById("social").innerHTML = "21hrs" ;
+  document.getElementById("socials").innerHTML = "last month- 23hrs" ;
+  document.getElementById("self").innerHTML = "2hrs" ;
+  document.getElementById("selfs").innerHTML = "lastweek- 2hrs" ;
+    
+}
+
